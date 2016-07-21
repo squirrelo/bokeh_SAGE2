9ab42a6f440c882f76e0996fdefcb131488608ab
Joshua Shorenstein <jshorens@gmail.com>
2016-07-21 17:20:28 -0600

    newer version of code

--- Bokeh_SAGE2.js
+++ Bokeh_SAGE2.js
@@ -18,13 +18,22 @@ var Bokeh_SAGE2 = SAGE2_App.extend( {
         // Create div into the DOM
         this.SAGE2Init("div", data);
         // Set the background to black
+        var idNum = Math.floor((Math.random() * 600000) + 1);
         this.element.style.backgroundColor = 'white';
-        this.element.id = "BokehPane"; +  Math.floor((Math.random() * 600000) + 1);
+        this.element.id = "BokehPane" + idNum;
         addCSS(this.resrcPath + "bootstrap.min.css");
         addCSS(this.resrcPath + "bokeh.css");
         this.server_url = this.state.server;
         this.bokehDoc = this.state.bokehDocument;
-        this.ident = this.element.id;
+        //Set up needed settings for bokeh
+        this.container_div = document.createElement('div');
+        this.container_div.style.height = "100%";
+        this.container_div.style.width = "100%";
+        this.container_div.style.margin = "0px";
+        this.container_div.classList.add("plotdiv");
+        this.container_div.id = "BokehInnerPane" + idNum;
+        this.ident = this.container_div.id;
+        this.element.appendChild(this.container_div);
         //var login_url = this.server_url + "bokeh/login?username=" + this.state.username + '&password=' + this.state.password;
         //var xhttp = new XMLHttpRequest();
         //xhttp.open("POST", login_url, false);
@@ -32,8 +41,7 @@ var Bokeh_SAGE2 = SAGE2_App.extend( {
 
         // move and resize callbacks
         this.server = "continuous";
-        this.moveEvents   = "continuous";
-
+        this.moveEvents = "continuous";
         // SAGE2 Application Settings
         //
         // Not adding controls but making the default buttons available
@@ -42,6 +50,7 @@ var Bokeh_SAGE2 = SAGE2_App.extend( {
 
     initializeWidgets: function() {
         this.controls.addTextInput({value: this.bokehDoc, position: 13, label: "Doc", identifier: "document"});
+        this.controls.addTextInput({value: this.server_url, position: 10, label: "Server", identifier: "server"});
         this.controls.finishedAddingControls();
         this.enableControls = true;
     },
@@ -61,32 +70,23 @@ var Bokeh_SAGE2 = SAGE2_App.extend( {
             var render_items = [{"elementid":ident,"sessionid":doc,"use_for_title":true}];
             Bokeh.embed.embed_items(docs_json, render_items, "ws://"+url+"/ws");
         });
-        window.source.trigger('change');
     },
 
     resize: function(date) {
-        window.source.trigger('change');
     },
 
     move: function(date) {
-        window.source.trigger('change');
-    },
-
-    quit: function() {
-        // Make sure to delete stuff (timers, ...)
-        Bokeh.terminate();
     },
 
     event: function(eventType, position, user_id, data, date) {
         if (eventType === "widgetEvent") {
             switch (data.identifier){
-                case "serverURL":
+                case "server":
                     this.server_url = data.text;
                     this.refresh(date);
                     break;
                 case "document":
                     this.bokehDoc = data.text;
-                    this.element.innerHTML = this.bokehDoc;
                     this.refresh(date);
                     break;
                 default:
